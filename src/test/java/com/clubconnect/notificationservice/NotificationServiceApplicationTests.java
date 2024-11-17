447173a7e69a7e73f14f357a39ae79212f65c26b
bhavindewani99 <bhavindewani@gmail.com>
2024-11-16 16:58:17 -0800

    Initial commit for notification and subscription

---
+++ src/test/java/com/clubconnect/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubconnect.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
