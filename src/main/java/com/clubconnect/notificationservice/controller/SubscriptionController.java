2d2378c2f1fb20159a5ff4c027f418d68c19191c
bhavindewani99 <bhavindewani@gmail.com>
2024-12-03 19:00:10 -0800

    Added cors

--- src/main/java/com/clubconnect/notificationservice/controller/SubscriptionController.java
+++ src/main/java/com/clubconnect/notificationservice/controller/SubscriptionController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 @RestController
 @RequestMapping("/api/subscriptions")
+@CrossOrigin(origins = "http://localhost:4200",allowedHeaders = "*")
 public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
